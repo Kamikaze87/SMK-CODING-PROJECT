7b6e7f711db1d871cc6391d8807749d235d100bf
Kamikaze87 <64583684+Kamikaze87@users.noreply.github.com>
2020-05-06 04:48:03 +0700

    Biodata App : CompleteBuild
    - Memperbaiki splash screen yang tidak muncul saat awal membuka aplikasi

--- SMKCodingProject1/app/src/main/java/com/example/smkcodingproject1/SplashActivity.kt
+++ SMKCodingProject1/app/src/main/java/com/example/smkcodingproject1/SplashActivity.kt
@@ -4,22 +4,16 @@ import android.os.Handler
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        window.requestFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-    Handler().postDelayed({
-        startActivity (Intent(this@SplashActivity, MainActivity::class.java))
-
-        finish()
-    },4000)
-
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+        },4000)
     }
 }
